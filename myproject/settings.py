a2d4497a4f493b2137feaebcf6264b93a55a19dc
Mike Kuriger <mikekuriger@gmail.com>
2024-11-07 11:02:02 -0600

    added /home/ssvm/ssvm/staticfiles

--- myproject/settings.py
+++ myproject/settings.py
@@ -224,7 +224,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
-
+STATIC_ROOT = "/home/ssvm/ssvm/staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
